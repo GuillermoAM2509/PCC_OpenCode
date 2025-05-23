d7f32b9d30ce79e25c41b14a6c60ace756ecb1ad
GuillermoAM2509 <g.aguilo@students.aula-ee.com>
2025-05-23 18:53:40 +0200

    Update PCC_streamlit_app.py

--- PCC_streamlit_app.py
+++ PCC_streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st 
 
 def calcular_consumo():
     st.title("TEST DE CONSUM ENERGÈTIC")
@@ -6,13 +6,13 @@ def calcular_consumo():
     
     pc_horas = st.slider("Hores d'ús ordenador:", 0, 12, 0)
     luces_horas = st.slider("Hores amb els llums encesos:", 0, 12, 0)
-    videoj_horas = st.slider("Hores jugant a videojocs: ", 0, 12, 0)
+    videoj_horas = st.slider("Hores jugant a videojocs:", 0, 12, 0)
     tv_horas = st.slider("Hores veient la televisió (Netflix, Prime i HBO conten, no t'amaguis):", 0, 12, 0)
     cafetera = st.slider("Hores d'ús de xarxes socials:", 0, 10, 0)
-    cargador_horas = st.slider("Horas carregant el telèfon mòbil:", 0, 10, 0)
+    cargador_horas = st.slider("Hores carregant el telèfon mòbil:", 0, 10, 0)
     nevera_var = st.checkbox("Freqüento la nevera / Tinc una a l'habitació (si homee...)")
-    ventilador_var = st.checkbox("Utilitzo ventilador/calefactor per dormir. ")
-    
+    ventilador_var = st.checkbox("Utilitzo ventilador/calefactor per dormir.")
+
     if st.button("Calcular Consum Beep Boop Beep"):
         consumo_total = (
             pc_horas * 0.2 +
@@ -30,21 +30,23 @@ def calcular_consumo():
         escuela_diario = 374806.1 / 365
         porcentaje = (consumo_total / escuela_diario) * 100
 
-        st.subheader(f" El teu consum diari aproximat es de -beep-boop-beep- {round(consumo_total, 2)} kWh!! ")
+        st.subheader(f"El teu consum diari aproximat és de {round(consumo_total, 2)} kWh.")
+        
         if consumo_total < 2:
             mensaje = "Ets el final boss de l'estalvi energètic! Consumeixes menys energia que un hobbit perdut al bosc (tant de bo ser tu)."
         elif 2 <= consumo_total < 5:
-            mensaje = "Consum mid, promig, no t'amagues i això està bé. Però sempre es pot millorar! "
+            mensaje = "Consum mid, promig, no t'amagues i això està bé. Però sempre es pot millorar!"
         else:
             mensaje = "Definitivament ens hem tornat bojos, relaxem-nos i recordem que la factura NO es pagarà sola."
         
         st.write(mensaje)
-        st.subheader(f" Comparat amb el consum diari de l'escola ({round(escuela_diario, 2)} kWh),  el teu consum representa el {round(porcentaje, 5)}%.")
+        
+        st.subheader(f"Comparat amb el consum diari de l'escola ({round(escuela_diario, 2)} kWh), el teu consum representa el {round(porcentaje, 5)}%.")
+        
         if porcentaje > 0.05:
-            extra_mensaje = "¡No està gens malament, però mai està de més intentar reduir-lo! Cada quilovat conta 💪. "
+            st.write("No està gens malament, però mai està de més intentar reduir-lo! Cada quilovat conta 💪.")
         else:
-            extra_mensaje = "Ets top efficiència, segueix així, que si jo fos tu estaria orgullós a muerte"
-        st.write(extra_mensaje)
+            st.write("Ets top eficiència, segueix així, que si jo fos tu estaria orgullós a muerte.")
 
 if __name__ == "__main__":
     calcular_consumo()
